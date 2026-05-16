687c91331467535dfc0418af5cc7e101b484d9b1
Rostik <rosticmel@gmail.com>
2026-05-17 00:03:47 +0300

    fix

--- app/notes/filter/page.tsx
+++
@@ -1,5 +0,0 @@
-import FilteredNotesPage from "./[...slug]/page";
-
-export default async function DefaultFilterPage() {
-  return <FilteredNotesPage params={Promise.resolve({ slug: ["all"] })} />;
-}
